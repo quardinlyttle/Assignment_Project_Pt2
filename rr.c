c3740027b7f0ce9afed71d53eab59fa311029757
Quardin Lyttle <lyttleqd@gmail.com>
2024-07-24 16:17:53 -0400

    first commit

---
+++ rr.c
@@ -0,0 +1,16 @@
+/* Family Name: Lyttle
+Given Name(s): Quardin
+Student Number: 215957889
+EECS Login ID: lyttleqd
+YorkU email address: lyttleqd@my.yorku.ca
+*/
+
+#include "sch-helpers.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
